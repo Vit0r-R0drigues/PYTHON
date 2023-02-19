a17c6b8113f39025f30bb0ca4f573d691057f206
Vit0r-R0drigues <vitorrodriguesdelima49@gmail.com>
2023-02-19 19:22:40 -0300

    app

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+
+app = QApplication(sys.argv)
+
+janela = QWidget()
+janela.resize(500, 500)
+janela.setWindowTitle('App One')
+janela.show()
+
+btn = QPushButton('Botao', janela)
+btn.setGeometry(300, 300, 150, 150)
+btn.setStyleSheet('background-color:red;color:white')
+
+label = QLabel('texto 1', janela)
+label.move(500, 200)
+label.setStyleSheet('background-color:black;color')
+
+app.exec()
